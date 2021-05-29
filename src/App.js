b80bfa479c8a1ed25433f756d060608f645e6039
Prasun <prasun.hazra@gmail.com>
2021-05-30 03:27:56 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './pic.jpeg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Services from './pages';
+import About from './pages/about';
+import Events from './pages/events';
+import AnnualReport from './pages/annual';
+import You from './pages/you';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact component={Services} />
+            <Route path='/about' component={About} />
+            <Route path='/events' component={Events} />
+            <Route path='/annual' component={AnnualReport} />
+            <Route path='/team' component={You} />
+            <Route path='/blogs' component={Blogs} />
+            <Route path='/sign-up' component={SignUp} />
+          </Switch>
+        </Router>
+             
+        
       </header>
-      <body>
-        <p>info@cognigocustoms.com</p>
+      <body className="App-body">
+        <div >
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
       </body>
       <footer>
+        
+        <div className="Right">info@cognigocustoms.com</div>
+    
         <p>
         The entirety of this site is protected by copyright © 2000–2021 Cognigo Customs, Inc.
         </p>
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './pic.jpeg';
+const Home = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Right',
+        alignItems: 'Right',
+        height: '100vh'
+      }}
+    >
+      <h1>Welcome to Cognigo Customs</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+};
+  
+export default Home;
