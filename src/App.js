a5ed333f9bfe3f448d38529db3f65dd9cb10481f
Prasun <prasun.hazra@gmail.com>
2021-05-30 16:18:14 +0530

    restore

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './pic.jpeg';
 import './App.css';
-import Navbar from './components/Navbar';
-import Services from './pages';
-import About from './pages/about';
-import Events from './pages/events';
-import AnnualReport from './pages/annual';
-import You from './pages/you';
-import Blogs from './pages/blogs';
-import SignUp from './pages/signup';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
-        <Router>
-          <Navbar />
-          <Switch>
-            <Route path='/' exact component={Services} />
-            <Route path='/about' component={About} />
-            <Route path='/events' component={Events} />
-            <Route path='/annual' component={AnnualReport} />
-            <Route path='/team' component={You} />
-            <Route path='/blogs' component={Blogs} />
-            <Route path='/sign-up' component={SignUp} />
-          </Switch>
-        </Router>
-             
+        <img src={logo} className="App-logo" alt="logo" />
         
       </header>
       <body className="App-body">
         <div >
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logoStatic" alt="logo" />
         </div>
       </body>
       <footer>
